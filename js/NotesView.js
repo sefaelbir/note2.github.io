2a270b5914b6287f926c863e9157b14370ad116a
Sefa Elbir <sefaelbir@gmail.com>
2022-04-01 03:22:57 +0300

    align all codes

--- js/NotesView.js
+++ js/NotesView.js
@@ -1,11 +1,14 @@
 export default class NotesView {
-    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
-        this.root = root;
-        this.onNoteSelect = onNoteSelect;
-        this.onNoteAdd = onNoteAdd;
-        this.onNoteEdit = onNoteEdit;
-        this.onNoteDelete = onNoteDelete;
-        this.root.innerHTML = `
+  constructor(
+    root,
+    { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}
+  ) {
+    this.root = root;
+    this.onNoteSelect = onNoteSelect;
+    this.onNoteAdd = onNoteAdd;
+    this.onNoteEdit = onNoteEdit;
+    this.onNoteDelete = onNoteDelete;
+    this.root.innerHTML = `
             <div class="notes__sidebar">
                 <button class="notes__add" type="button">Not Ekle</button>
                 <div class="notes__list"></div>
@@ -16,31 +19,31 @@ export default class NotesView {
             </div>
         `;
 
-        const btnAddNote = this.root.querySelector(".notes__add");
-        const inpTitle = this.root.querySelector(".notes__title");
-        const inpBody = this.root.querySelector(".notes__body");
+    const btnAddNote = this.root.querySelector(".notes__add");
+    const inpTitle = this.root.querySelector(".notes__title");
+    const inpBody = this.root.querySelector(".notes__body");
 
-        btnAddNote.addEventListener("click", () => {
-            this.onNoteAdd();
-        });
+    btnAddNote.addEventListener("click", () => {
+      this.onNoteAdd();
+    });
 
-        [inpTitle, inpBody].forEach(inputField => {
-            inputField.addEventListener("blur", () => {
-                const updatedTitle = inpTitle.value.trim();
-                const updatedBody = inpBody.value.trim();
+    [inpTitle, inpBody].forEach((inputField) => {
+      inputField.addEventListener("blur", () => {
+        const updatedTitle = inpTitle.value.trim();
+        const updatedBody = inpBody.value.trim();
 
-                this.onNoteEdit(updatedTitle, updatedBody);
-            });
-        });
+        this.onNoteEdit(updatedTitle, updatedBody);
+      });
+    });
 
-        this.updateNotePreviewVisibility(false);
-    }
+    this.updateNotePreviewVisibility(false);
+  }
 
-    _createListItemHTML(id, title, body, updated) {
-        const MAX_TITLE_LENGTH = 20;
-        const MAX_BODY_LENGTH = 60;
-        
-        return `
+  _createListItemHTML(id, title, body, updated) {
+    const MAX_TITLE_LENGTH = 20;
+    const MAX_BODY_LENGTH = 60;
+
+    return `
             <div class="notes__list-item" data-note-id="${id}">
                 <div class="notes__small-title">
                 ${title.substring(0, MAX_TITLE_LENGTH)}
@@ -52,52 +55,66 @@ export default class NotesView {
                     ${body.length > MAX_BODY_LENGTH ? "..." : ""}
                 </div>
                 <div class="notes__small-updated">
-                    ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
+                    ${updated.toLocaleString(undefined, {
+                      dateStyle: "full",
+                      timeStyle: "short",
+                    })}
                 </div>
             </div>
         `;
+  }
+
+  updateNoteList(notes) {
+    const notesListContainer = this.root.querySelector(".notes__list");
+
+    // boş liste
+    notesListContainer.innerHTML = "";
+
+    for (const note of notes) {
+      const html = this._createListItemHTML(
+        note.id,
+        note.title,
+        note.body,
+        new Date(note.updated)
+      );
+
+      notesListContainer.insertAdjacentHTML("beforeend", html);
     }
 
-    updateNoteList(notes) {
-        const notesListContainer = this.root.querySelector(".notes__list");
-
-        // boş liste
-        notesListContainer.innerHTML = "";
-
-        for (const note of notes) {
-            const html = this._createListItemHTML(note.id, note.title, note.body, new Date(note.updated));
-
-            notesListContainer.insertAdjacentHTML("beforeend", html);
-        }
-
-        // ekleme silme
-        notesListContainer.querySelectorAll(".notes__list-item").forEach(noteListItem => {
-            noteListItem.addEventListener("click", () => {
-                this.onNoteSelect(noteListItem.dataset.noteId);
-            });
-
-            noteListItem.addEventListener("dblclick", () => {
-                const doDelete = confirm("Silmek istediğine emin misin?");
-
-                if (doDelete) {
-                    this.onNoteDelete(noteListItem.dataset.noteId);
-                }
-            });
-        });
-    }
-
-    updateActiveNote(note) {
-        this.root.querySelector(".notes__title").value = note.title;
-        this.root.querySelector(".notes__body").value = note.body;
-
-        this.root.querySelectorAll(".notes__list-item").forEach(noteListItem => {
-            noteListItem.classList.remove("notes__list-item--selected");
+    // ekleme silme
+    notesListContainer
+      .querySelectorAll(".notes__list-item")
+      .forEach((noteListItem) => {
+        noteListItem.addEventListener("click", () => {
+          this.onNoteSelect(noteListItem.dataset.noteId);
         });
 
-        this.root.querySelector(`.notes__list-item[data-note-id="${note.id}"]`).classList.add("notes__list-item--selected");
-    }
+        noteListItem.addEventListener("dblclick", () => {
+          const doDelete = confirm("Silmek istediğine emin misin?");
 
-    updateNotePreviewVisibility(visible) {
-        this.root.querySelector(".notes__preview").style.visibility = visible ? "visible" : "hidden";
-    }
+          if (doDelete) {
+            this.onNoteDelete(noteListItem.dataset.noteId);
+          }
+        });
+      });
+  }
+
+  updateActiveNote(note) {
+    this.root.querySelector(".notes__title").value = note.title;
+    this.root.querySelector(".notes__body").value = note.body;
+
+    this.root.querySelectorAll(".notes__list-item").forEach((noteListItem) => {
+      noteListItem.classList.remove("notes__list-item--selected");
+    });
+
+    this.root
+      .querySelector(`.notes__list-item[data-note-id="${note.id}"]`)
+      .classList.add("notes__list-item--selected");
+  }
+
+  updateNotePreviewVisibility(visible) {
+    this.root.querySelector(".notes__preview").style.visibility = visible
+      ? "visible"
+      : "hidden";
+  }
 }
--- js/main.js
+++ js/main.js
@@ -3,15 +3,15 @@ import App from "./App.js";
 const root = document.getElementById("app");
 const app = new App(root);
 
-const toggle = document.getElementById('toggle');
+const toggle = document.getElementById("toggle");
 const body = document.body;
 
-toggle.addEventListener('input', (e) => {
-	const isChecked = e.target.checked;
-	
-	if(isChecked) {
-		body.classList.add('light-theme');
-	} else {
-		body.classList.remove('light-theme');
-	}
+toggle.addEventListener("input", (e) => {
+  const isChecked = e.target.checked;
+
+  if (isChecked) {
+    body.classList.add("light-theme");
+  } else {
+    body.classList.remove("light-theme");
+  }
 });
